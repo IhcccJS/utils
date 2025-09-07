a606c34367f24580b9767e73aba97d866a8050f0
ihccc <lccc10588@gmail.com>
2025-09-08 00:48:26 +0800

    Update .fatherrc.ts

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'es' },
-  cjs: { output: 'lib' },
+  esm: { output: 'es', ignores: ['src/**/demo/**/*', 'src/components/**'] },
+  cjs: { output: 'lib', ignores: ['src/**/demo/**/*', 'src/components/**'] },
   umd: { name: 'wtools', output: 'dist' },
 });
